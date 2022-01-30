c1cd1baa521c64c2a81456a8a0fe6cdb303f65bc
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-01-30 22:55:36 +0530

    All works are finished now its ready to take android release. So we need to genarate key for publish in playstore and some editing in android file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/controller/button_controller.dart';
+import 'package:flutter_calculator/screenhome.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const CalculatorApp());
 }
+
 class CalculatorApp extends StatelessWidget {
-  const CalculatorApp({ Key? key }) : super(key: key);
+  const CalculatorApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // hide debug banner in emulator
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => ButtonController(),
+      child: const MaterialApp(
+        // hide debug banner in emulator
+        debugShowCheckedModeBanner: false,
+        home: ScreenHome(),
+      ),
     );
   }
 }
