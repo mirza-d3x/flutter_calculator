5bcdcb1056417a086c9c33299a49f3f085e1c3e1
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-01-30 21:36:38 +0530

    Button list created and custom button widget created using google fonts to all buttons

---
+++ lib/widget/buttonList.dart
@@ -0,0 +1,29 @@
+class ButtonModel {
+  final List<String> buttons = [
+    "AC",
+    "C",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "00",
+    "0",
+    ".",
+    "=",
+  ];
+}
+/*
+Button list model 
+button will show by the index of list.
+we can indivitualy change button function 
+using index numbers in if else */
--- lib/widget/calcubutton.dart
+++ lib/widget/calcubutton.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({ Key? key }) : super(key: key);
+  CustomButton(
+      {Key? key,
+      required this.isEqual,
+      required this.onTap,
+      required this.text,
+      this.buttonColor,
+      this.textColor})
+      : super(key: key);
+
+  final void Function() onTap;
+  final String text;
+  final Color? buttonColor;
+  final Color? textColor;
+  bool isEqual = true;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          width: 60,
+          height: 60,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(color: buttonColor),
+          child: Text(
+            text,
+            style: isEqual
+                // equal button
+                ? GoogleFonts.rubik(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: textColor,
+                  )
+                :
+                // all buttons excluded equal button
+                GoogleFonts.rubik(
+                    fontSize: 40,
+                    color: textColor,
+                  ),
+          ),
+        ),
+      ),
     );
   }
 }
